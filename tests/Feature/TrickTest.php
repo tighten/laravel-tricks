705b2f16608eed4252a9b491dc674a88184e14d6
Andy Newhouse <hi@andymnewhouse.me>
2024-04-24 16:02:59 -0500

    Add test for viewing create trick page

--- tests/Feature/TrickTest.php
+++ tests/Feature/TrickTest.php
@@ -1,12 +1,24 @@
 <?php
 
 use App\Models\Trick;
+use App\Models\User;
 
 it('can view tricks on homepage', function () {
     Trick::factory()->create(['name' => 'How to make a trick']);
 
-    $this->withoutExceptionHandling()
-        ->get('/')
+    $this->get(route('tricks.index'))
         ->assertStatus(200)
         ->assertSee('How to make a trick');
+});
+
+it('can view create trick page', function () {
+    $this->actingAs(User::factory()->create())
+        ->get(route('tricks.create'))
+        ->assertStatus(200)
+        ->assertSee('Create Trick');
+});
+
+it('cannot view create trick page as guest', function () {
+    $this->get(route('tricks.create'))
+        ->assertStatus(302);
 });
