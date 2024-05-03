aefd819ec0475057275ba7b1b553e4df7fa08d40
Andy Newhouse <hi@andymnewhouse.me>
2024-05-03 12:21:03 -0500

    Update container component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,9 +21,9 @@
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <x-container class="py-6">
                         {{ $header }}
-                    </div>
+                    </x-container>
                 </header>
             @endif
 
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <x-container class="space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
@@ -24,6 +24,6 @@
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
-        </div>
+        </x-container>
     </div>
 </x-app-layout>
